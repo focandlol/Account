11519c1af74ae6bf12ad3c551c67535982688274
focandlol <focandlol@naver.com>
2024-11-25 18:58:57 +0900

    th

--- src/main/java/org/example/account/repository/AccountRepository.java
+++ src/main/java/org/example/account/repository/AccountRepository.java
@@ -3,7 +3,6 @@ package org.example.account.repository;
 import org.example.account.domain.Account;
 import org.example.account.domain.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
